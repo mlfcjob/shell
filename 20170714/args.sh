f822762d11a3f095bce42645cb8450a10c73670a
mlfcjob@163.com <mlfcjob@163.com>
2017-07-14 10:11:21 +0800

    Demo of usage of command line.
    Demo of usage of command line.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

---
+++ 20170714/args.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#bash shell args
+
+echo ====================================
+echo "read the arguments"
+
+factorial=1
+
+for ((number = 1; number <= $1; number++))
+do
+    factorial=$[ $factorial * $number ]
+done
+echo "The factorial of $1 is $factorial"
+
---
+++ 20170714/over_nine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ========================
+echo "Over 9 args sh"
+
+total=$[ ${10} * ${11} ]
+echo "total $total"
+
