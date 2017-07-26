741ca42c094503cbe336d9eb8f251843aae583db
mlfcjob@163.com <mlfcjob@163.com>
2017-07-26 15:41:22 +0800

    Add some file of branch 20170713.
    Add some files of branch 20170713.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

---
+++ 20170713/exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ==================================
+echo "get bash shell name from command line"
+
+echo "The command bash shell exec is $0 "
---
+++ 20170713/mul.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name=`basename $0`
+
+if [ $name == "add.sh" ]
+then
+    total=$[ $1 + $2 ]
+elif [ $name == "mul.sh" ]
+then
+    total=$[ $1 * $2 ]
+fi
+
+echo "The result is $total"
