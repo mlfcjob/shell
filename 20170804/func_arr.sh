32c69a57f9fd9930d4de9ad7d64412b037b02044
mlfcjob@163.com <mlfcjob@163.com>
2017-08-04 12:05:10 +0800

    A demo of arr usage in a function.
    A demo of arr usage in a function.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

---
+++ 20170804/func_arr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# trying to pass an array variable
+
+function testarr {
+    echo "The parameters are: $@"
+	thisarray=$1
+	echo "The received array is ${thisarray[*]}"
+}
+
+myarray=(1 2 3 4 5)
+echo "The original array is: ${myarray[*]}"
+
+testarr $myarray
